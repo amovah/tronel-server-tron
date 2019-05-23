2a61e6cd8398b0c384e88fb19f5c1505ba5e25b8
Ali Movahedi <ali_movahedi@aol.com>
2019-05-23 10:38:55 +0430

    feat: setup mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express';
 import { env } from 'process';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/tronel', { useNewUrlParser: true });
+mongoose.connection.on('error', (error) => {
+  console.error(`Database connection error ${error}`);
+
+  process.exit(0);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.error('Disconnected from database');
+
+  process.exit(0);
+});
+
 
 const app = express();
 
