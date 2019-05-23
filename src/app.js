082758b36b23580dd940d0dafba9993c4b8a4bdd
Ali Movahedi <ali_movahedi@aol.com>
2019-05-23 10:35:41 +0430

    feat: setup parser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { env } from 'process';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(env.NODE_PORT || 8010, () => {
   console.log(`server starts at ${env.NODE_PORT || 8010}`);
