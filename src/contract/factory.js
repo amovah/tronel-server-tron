e6a80fff3bf87ebc1e999c938f0313fd36e35a04
Ali Movahedi <ali_movahedi@aol.com>
2019-06-16 14:04:04 +0430

    feat: update factory address

--- src/contract/factory.js
+++ src/contract/factory.js
@@ -1,4 +1,4 @@
 import web3 from 'Root/web3';
 import abi from 'Root/abi/Factory.json';
 
-export default web3.pi.contract(abi).at('0x08c8eb28727037a2b17e82106003f1510d1cb9d6');
+export default web3.pi.contract(abi).at('0x3ff0251e8d86856318f3a1a083d4b0f84b953171');
