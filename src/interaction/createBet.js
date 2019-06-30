63e61ba0e3fa0153d5702d554f9d30ff2a4fb1d7
Ali Movahedi <ali_movahedi@aol.com>
2019-06-30 12:55:03 +0430

    feat: add bet api
    fix: fix creatation bet

--- src/interaction/createBet.js
+++ src/interaction/createBet.js
@@ -1,16 +1,19 @@
-import loadFactory from 'Root/contracts/factory';
+import tronweb from 'Root/tronweb';
+import betadata from 'Root/betdata.json';
 
 async function app() {
-  const factory = await loadFactory();
-
-  const res = await factory.createBet(
-    'bitcoin',
-    100,
-    Math.floor(Date.now() / 1000 + 100000),
-    1,
-  ).send({
-    // feeLimit: 1000,
-    shouldPollResponse: true,
+  const res = await tronweb.contract().new({
+    ...betadata,
+    feeLimit: 1000000000,
+    userFeePercentage: 1,
+    callValue: 100,
+    parameters: [
+      process.env.MANAGER,
+      'bitcoin',
+      100,
+      Math.floor(Date.now() / 1000 + 1000),
+      1,
+    ],
   });
 
   console.log(res);
