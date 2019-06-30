b8e659e391702cca75c1c122ce2ddfff3ad1c0d1
Ali Movahedi <ali_movahedi@aol.com>
2019-06-30 13:02:28 +0430

    remove: remove joinAsCreator route

--- src/routers/bets/index.js
+++ src/routers/bets/index.js
@@ -2,12 +2,10 @@ import post from './post';
 import get from './get';
 import join from './join';
 import disable from './disable';
-import joinAsCreator from './joinAsCreator';
 
 export default [
   post,
   get,
   join,
   disable,
-  joinAsCreator,
 ];
