6fd5ceb02a9370ad3ebd5e2c1f1b31e5bc0fe948
Ali Movahedi <ali_movahedi@aol.com>
2019-07-01 12:57:24 +0430

    refactor: refactor retrieveMoney

--- src/helpers/retrieveMoney.js
+++ src/helpers/retrieveMoney.js
@@ -1,3 +1,21 @@
-export default async (address) => {
-  // call retrieve method on bet contract
+import tronweb from 'Roo/tronweb';
+import Bet from 'Root/models/Bet';
+
+export default async (betId) => {
+  try {
+    const bet = await Bet.findById(betId);
+
+    const betContract = await tronweb.contract().at(bet.address);
+    await betContract.retrieveMoney().send({
+      shouldPollResponse: true,
+    });
+
+    bet.done = await betContract.done().call();
+    await bet.save();
+  } catch (e) {
+    console.error(
+      `bet ${betId} went wrong on retrieveMoney`,
+      e,
+    );
+  }
 };
