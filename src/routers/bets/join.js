aaa5df11a349aaffa895fae913b2c736f7b6a336
Ali Movahedi <ali_movahedi@aol.com>
2019-07-01 12:32:58 +0430

    refactor: refactor join route

--- src/routers/bets/join.js
+++ src/routers/bets/join.js
@@ -1,32 +1,35 @@
 import { Router } from 'express';
+import { stop } from 'Root/job';
 import Bet from 'Root/models/Bet';
-// import { stop } from 'Root/job';
+import tronweb from 'Root/tronweb';
 
 const router = new Router();
 
 router.put('/bets/:id/join', async (req, res) => {
-  // try {
-  //   const bet = await Bet.findById(req.params.id);
-  //   if (!bet) {
-  //     throw new Error('Bet not found!');
-  //   }
-  //
-  //   const joiner = betContract.joiner();
-  //   if (joiner !== '0x0000000000000000000000000000000000000000') {
-  //     bet.joiner = joiner;
-  //     bet.save();
-  //
-  //     // stop(`${bet.id.toString()}-retrieve`);
-  //   }
-  //
-  //   res.json(bet);
-  // } catch (e) {
-  //   res.status(400);
-  //   res.json({
-  //     status: 'error',
-  //     error: e,
-  //   });
-  // }
+  try {
+    const bet = await Bet.findById(req.params.id);
+    if (!bet) {
+      throw new Error('Bet not found!');
+    }
+
+    const betContract = await tronweb.contract().at(bet.address);
+    const joiner = await betContract.joiner().call();
+
+    if (tronweb.address.fromHex(joiner) !== 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb') {
+      bet.joiner = joiner;
+      bet.save();
+
+      stop(`${bet.id}-retrieveMoney`);
+    }
+
+    res.json(bet);
+  } catch (e) {
+    res.status(400);
+    res.json({
+      status: 'error',
+      error: e,
+    });
+  }
 });
 
 export default router;
