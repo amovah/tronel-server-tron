1fc46180b290da1f4b1618b92d3872d78654ceed
Ali Movahedi <ali_movahedi@aol.com>
2019-07-08 11:29:35 +0430

    feat: update server environment vars

--- src/interaction/create.js
+++ src/interaction/create.js
@@ -2,20 +2,26 @@ import tronweb from 'Root/tronweb';
 import betadata from 'Root/betdata.json';
 
 async function app() {
-  const res = await tronweb.contract().new({
-    ...betadata,
-    userFeePercentage: 1,
-    callValue: 307 * 1000000,
-    parameters: [
-      process.env.MANAGER,
-      'bitcoin',
-      100,
-      Math.floor(Date.now() / 1000) + 180,
-      1,
-    ],
-  });
+  // const contract = await tronweb.contract().at('TVpWcabE46Sj3FcdNnQ3yf1Vmt5t4gm4vg');
+  //
+  // const res = await contract.createBet(
+  //   "bitcoin",
+  //   1000,
+  //   Math.floor(Date.now() / 1000 + 500),
+  //   Math.floor(Date.now() / 1000 + 400),
+  //   1,
+  //   25 * 1000000,
+  // ).send({
+  //   callValue: 25 * 1000000,
+  //   shouldPollResponse: true,
+  // });
+  //
+  // console.log(res);
+  const contract = await tronweb.contract().at('TVpWcabE46Sj3FcdNnQ3yf1Vmt5t4gm4vg');
 
-  console.log(tronweb.address.fromHex(res.address));
+  const res = await contract.getSummary(0).call();
+
+  console.log(res);
 }
 
 app();
