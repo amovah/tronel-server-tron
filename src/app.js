2b2f48aff689a2c6b070491ebd570ab082d2d75e
Ali Movahedi <ali_movahedi@aol.com>
2019-07-08 18:55:04 +0430

    fix: fix response headers

--- src/app.js
+++ src/app.js
@@ -51,6 +51,7 @@ mongoose.connection.on('disconnected', () => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
     next();
   });
